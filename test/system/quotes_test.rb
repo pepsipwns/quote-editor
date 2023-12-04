73799700ecc73e205e1b47e809365541b71e50af
Anthony <anthony.brown@krystal.uk>
2023-12-04 16:51:47 +0000

    Code cleanup & tests now all fixed

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -4,7 +4,8 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first)
+    # We need to order quote as well in the system tests
+    @quote = Quote.ordered.first
   end
 
   test "Showing a quote" do
